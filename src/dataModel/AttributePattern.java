db06917688f40bcfee5da46ce64f80459eea0722
moloch42 <djtickner@gmail.com>
2015-01-23 10:00:49 -0400

    Implement the patternmatch method of AttributePattern. Addresses Issue #12.

--- src/dataModel/AttributePattern.java
+++ src/dataModel/AttributePattern.java
@@ -27,9 +27,16 @@ public class AttributePattern {
      */
     public AttributePattern(String elementName, String elementAttributeName,
                             String elementAttributeValue) throws AttributePatternException {
-            this.element_name = elementName;
-            this.element_attribute_name = elementAttributeName;
-            this.element_attribute_value = elementAttributeValue;
+        if (elementName == null) {
+            throw new AttributePatternException("element_name can not be null");
+        }
+        if (elementAttributeName == null && elementAttributeValue != null) {
+            throw new AttributePatternException("You can not have an element_attribute_value without an element_attribute_name");
+        }
+
+        this.element_name = elementName;
+        this.element_attribute_name = elementAttributeName;
+        this.element_attribute_value = elementAttributeValue;
         }
 
 
@@ -62,9 +69,57 @@ public class AttributePattern {
      * @return all subtags of the given tag that match this pattern
      */ 
         public List<TagNode> patternMatch(TagNode node) {
-            //TODO Implement This method
-            //given a TagNode, return the child node(s?) that match this pattern or empty list if nothing matches
-            return new LinkedList<TagNode>();
+
+            List<TagNode> matchingNodes = new LinkedList<TagNode>();
+
+            //Get all of the nodes that match
+//            TagNode[] nodes = node.getElementsByName(element_name, false);
+            
+            //get all direct child nodes that match the tag type
+            List<TagNode> nodes = node.getElementListByName(element_name, false);
+
+            
+            
+            for (TagNode t: nodes) {
+            	//skip this node if the attribute name doesn't match
+            	if (element_attribute_name != null && !t.hasAttribute(element_attribute_name)) {
+            		continue;
+            	}
+            	//skip this node if the attribute value doesn't match
+            	if (element_attribute_value != null && !t.getAttributeByName(element_attribute_name).equals(element_attribute_value)) {
+            		continue;
+            	}
+            	
+            	//the node matches, add it to the list
+            	matchingNodes.add(t);
+            }
+            
+            
+            
+//            //if there is a required attribute name then we only want to include those nodes
+//            //if there is not then we can include all nodes
+//            if (element_attribute_name != null) {
+//                for (TagNode t: nodes) {
+//
+//                    //if the attribute value is not null then we only want to include this node if it matches
+//                    //otherwise include this node by default
+//                    if (element_attribute_value != null) {
+//                    	String attributeValue = t.getAttributeByName(element_attribute_name);
+//                        if (attributeValue != null && attributeValue.equals(element_attribute_value)) {
+//                            matchingNodes.add(t);
+//                        }
+//
+//                    } else {
+//                        matchingNodes.add(t);
+//                    }
+//                }
+//            } else {
+//            	for (TagNode t: nodes) {
+//                    matchingNodes.add(t);
+//                }
+//            }
+
+            return matchingNodes;
         }
 
 

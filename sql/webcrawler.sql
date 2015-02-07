f267b0060668077da1a276b0ac866ac7047d5cfe
Michael Power <vaultdweller@gmail.com>
2015-02-07 01:18:15 -0400

    Updated the SQL Script to reflect the new database design (issue #1) and added an item_details view to easily check crawled data

--- sql/webcrawler.sql
+++ sql/webcrawler.sql
@@ -1,63 +1,73 @@
-
-CREATE SCHEMA IF NOT EXISTS `webcrawler`;
-USE `webcrawler` ;
-
-DROP TABLE IF EXISTS `webcrawler`.`item_attribute`;
-DROP TABLE IF EXISTS `webcrawler`.`item`;
-DROP TABLE IF EXISTS `webcrawler`.`attribute_name`;
-DROP TABLE IF EXISTS `webcrawler`.`seller`;
+DROP SCHEMA IF EXISTS webcrawler;
+CREATE SCHEMA webcrawler;
+USE webcrawler ;
 
 -- -----------------------------------------------------
--- Table `webcrawler`.`attribute_name`
+-- Table webcrawler.attribute_name
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `webcrawler`.`attribute_name` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(64) NOT NULL,
-  `data_type` VARCHAR(64) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`));
+CREATE TABLE attribute_name (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  name VARCHAR(64) NOT NULL,
+  data_type VARCHAR(64) NULL DEFAULT NULL,
+  PRIMARY KEY (id));
 
 
 -- -----------------------------------------------------
--- Table `webcrawler`.`seller`
+-- Table webcrawler.seller
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `webcrawler`.`seller` (
-  `seller_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(512) NULL DEFAULT NULL,
-  PRIMARY KEY (`seller_id`));
+CREATE TABLE seller (
+  seller_id INT(11) NOT NULL AUTO_INCREMENT,
+  name VARCHAR(512) NULL DEFAULT NULL,
+  PRIMARY KEY (seller_id));
 
 
 -- -----------------------------------------------------
--- Table `webcrawler`.`item`
+-- Table webcrawler.item
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `webcrawler`.`item` (
-  `item_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `seller_id` INT(11) NOT NULL,
-  `active_flag` TINYINT(1) NULL DEFAULT NULL,
-  PRIMARY KEY (`item_id`),
-  INDEX `seller_id` (`seller_id` ASC),
-  CONSTRAINT `item_ibfk_1`
-    FOREIGN KEY (`seller_id`)
-    REFERENCES `webcrawler`.`seller` (`seller_id`));
+CREATE TABLE item (
+  item_id INT(11) NOT NULL AUTO_INCREMENT,
+  seller_id INT(11) NOT NULL,
+  active_flag TINYINT(1) NULL DEFAULT NULL,
+  PRIMARY KEY (item_id),
+  INDEX seller_id (seller_id ASC),
+  CONSTRAINT item_fk_seller
+    FOREIGN KEY (seller_id)
+    REFERENCES seller (seller_id));
 
 
 -- -----------------------------------------------------
--- Table `webcrawler`.`item_attribute`
+-- Table webcrawler.item_attribute
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `webcrawler`.`item_attribute` (
-  `attribute_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `item_id` INT(11) NOT NULL,
-  `attribute_name_fk` INT(11) NOT NULL,
-  `attribute_value` TEXT NULL DEFAULT NULL,
-  PRIMARY KEY (`attribute_id`, `item_id`),
-  INDEX `attribute_name_fk` (`attribute_name_fk` ASC),
-  CONSTRAINT `item_attribute_ibfk_1`
-    FOREIGN KEY (`attribute_name_fk`)
-    REFERENCES `webcrawler`.`attribute_name` (`id`)
+CREATE TABLE item_attribute (
+  attribute_id INT(11) NOT NULL AUTO_INCREMENT,
+  item_id INT(11) NOT NULL,
+  attribute_name_fk INT(11) NOT NULL,
+  attribute_value TEXT NULL DEFAULT NULL,
+  PRIMARY KEY (attribute_id, item_id),
+  INDEX attribute_name_fk (attribute_name_fk ASC),
+  CONSTRAINT item_attribute_fk_attribute_name
+    FOREIGN KEY (attribute_name_fk)
+    REFERENCES attribute_name (id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `item_attribute_ibfk_2`
-    FOREIGN KEY (`item_id`)
-    REFERENCES `webcrawler`.`item` (`item_id`)
+  CONSTRAINT item_attribute_fk_item
+    FOREIGN KEY (item_id)
+    REFERENCES item (item_id)
 	ON DELETE CASCADE
 	ON UPDATE CASCADE
 );
+
+
+-- -----------------------------------------------------
+-- View webcrawler.item_details
+-- -----------------------------------------------------
+CREATE VIEW item_details AS
+	SELECT item.item_id, seller.name seller, an.name attribute, ia.attribute_value value
+	FROM seller
+    JOIN item
+		ON seller.seller_id = item.seller_id
+	JOIN item_attribute ia
+		ON item.item_id = ia.item_id
+	JOIN attribute_name an
+		ON ia.attribute_name_fk = an.id
+;
